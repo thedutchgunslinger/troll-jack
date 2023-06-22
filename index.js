2aa573e7d360afe17b11cf27d48c6626a33f294b
Noah Beij <36964954+thedutchgunslinger@users.noreply.github.com>
2023-06-22 12:41:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const http = require("http");
+const http = require("https");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
